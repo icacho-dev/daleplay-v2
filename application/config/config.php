c25066e0d7c38ff3fa60e0eec61b00748315e847
oterojc <oterojc@gmail.com>
2015-02-27 00:59:12 -0600

    host fixed

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/ci22/';
+$config['base_url']	= 'http://swfideas.com/ci22/';
 
 /*
 |--------------------------------------------------------------------------
