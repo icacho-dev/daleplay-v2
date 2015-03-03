105e738aeba62d202ea869301f93d1221aaf392a
oterojc <oterojc@gmail.com>
2015-03-03 12:07:54 -0600

    Header Load Fancy Modal Fixed

--- application/views/includes/adminheader.php
+++ application/views/includes/adminheader.php
@@ -30,7 +30,6 @@
     <![endif]-->
 
     <!-- ######### CSS STYLES ######### -->
-
     <link rel="stylesheet" href="<?php echo base_url();?>css/reset.css" type="text/css" />
     <link rel="stylesheet" href="<?php echo base_url();?>css/style.css" type="text/css" />
     <link rel="stylesheet" href="<?php echo base_url();?>css/dialogs.css" type="text/css" />
@@ -50,65 +49,29 @@
     <!-- shortcodes -->
     <link rel="stylesheet" media="screen" href="<?php echo base_url();?>css/shortcodes.css" type="text/css" />
 
-<!-- just remove the below comments witch color skin you want to use -->
-    <!--<link rel="stylesheet" href="css/colors/blue.css" />-->
-    <!--<link rel="stylesheet" href="css/colors/green.css" />-->
-    <!--<link rel="stylesheet" href="css/colors/cyan.css" />-->
-    <!--<link rel="stylesheet" href="css/colors/orange.css" />-->
-    <!--<link rel="stylesheet" href="css/colors/lightblue.css" />-->
-    <!--<link rel="stylesheet" href="css/colors/pink.css" />-->
-    <!--<link rel="stylesheet" href="css/colors/purple.css" />-->
-    <!--<link rel="stylesheet" href="css/colors/bridge.css" />-->
-    <!--<link rel="stylesheet" href="css/colors/slate.css" />-->
-    <!--<link rel="stylesheet" href="css/colors/yellow.css" />-->
-    <!--<link rel="stylesheet" href="css/colors/darkred.css" />-->
-
-<!-- just remove the below comments witch bg patterns you want to use -->
-    <!--<link rel="stylesheet" href="css/bg-patterns/pattern-default.css" />-->
-    <!--<link rel="stylesheet" href="css/bg-patterns/pattern-one.css" />-->
-    <!--<link rel="stylesheet" href="css/bg-patterns/pattern-two.css" />-->
-    <!--<link rel="stylesheet" href="css/bg-patterns/pattern-three.css" />-->
-    <!--<link rel="stylesheet" href="css/bg-patterns/pattern-four.css" />-->
-    <!--<link rel="stylesheet" href="css/bg-patterns/pattern-five.css" />-->
-    <!--<link rel="stylesheet" href="css/bg-patterns/pattern-six.css" />-->
-    <!--<link rel="stylesheet" href="css/bg-patterns/pattern-seven.css" />-->
-    <!--<link rel="stylesheet" href="css/bg-patterns/pattern-eight.css" />-->
-    <!--<link rel="stylesheet" href="css/bg-patterns/pattern-nine.css" />-->
-    <!--<link rel="stylesheet" href="css/bg-patterns/pattern-ten.css" />-->
-    <!--<link rel="stylesheet" href="css/bg-patterns/pattern-eleven.css" />-->
-    <!--<link rel="stylesheet" href="css/bg-patterns/pattern-twelve.css" />-->
-    <!--<link rel="stylesheet" href="css/bg-patterns/pattern-thirteen.css" />-->
-
     <!-- progressbar -->
     <link rel="stylesheet" href="<?php echo base_url();?>js/progressbar/ui.progress-bar.css">
 
     <!-- mega menu-->
-    <!-- <link href="<?php echo base_url();?>js/mainmenu/bootstrap.min.css" rel="stylesheet">-->
     <link href="<?php echo base_url();?>js/mainmenu/menu.css" rel="stylesheet">
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 
-
     <!-- forms -->
     <link rel="stylesheet" href="<?php echo base_url();?>js/form/sky-forms2.css" type="text/css" media="all">
-
     <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>js/tabs/assets/css/responsive-tabs.css">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>js/tabs/assets/css/responsive-tabs2.css">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>js/tabs/assets/css/responsive-tabs3.css">
 
-    <!-- angular-modal
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>js/angular-fancy-modal/angular-fancy-modal.min.css"> -->
-
-    <!-- angular app https://code.angularjs.org/1.2.28/ -->
+    <!-- angular-modal -->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>js/angular-fancy-modal/angular-fancy-modal.min.css">
     <script src="//code.angularjs.org/1.2.13/angular.js"></script>
     <script src="//code.angularjs.org/1.2.25/angular-sanitize.min.js"></script>
   	<script src="<?php echo base_url();?>js/app/ui-bootstrap-tpls-0.12.0.min.js"></script>
-
     <script src="//cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.8/angular-ui-router.min.js"></script>
   	<script src="<?php echo base_url();?>js/app/angular-timestamp-filter.js"></script>
   	<script src="<?php echo base_url();?>js/angular-fancy-modal/angular-fancy-modal.min.js"></script>
   	<script src="<?php echo base_url();?>js/angular-file-upload/angular-file-upload-shim.min.js"></script>
   	<script src="<?php echo base_url();?>js/angular-file-upload/angular-file-upload.min.js"></script>
-
     <script src="<?php echo base_url();?>js/app/dialogs.min.js"></script>
   	<script src="<?php echo base_url();?>js/app/app.js"></script>
   	<script src="<?php echo base_url();?>js/app/controllers.js"></script>
