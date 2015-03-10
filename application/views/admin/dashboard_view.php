0bc81443eb315ed1e261b757f14e2733b1850985
Isaac Cacho G. <msn@swfideas.com>
2015-03-10 16:51:16 -0600

    dashboard admin & file download @contents

--- application/views/admin/dashboard_view.php
+++ application/views/admin/dashboard_view.php
@@ -1,25 +1,1 @@
-<div class="clearfix"></div>
-<div class="parallax_section6">
-<div class="container">
-
-	<h2>Admin Dashboard</h2>
-        
-	<div class="box">
-        
-        <div class="hexagon"><i class="fa fa-check-square-o"></i> <h6>Categorias</h6> <p>15 categorias</p></div>
-        <div class="hexagon"><i class="fa fa-paper-plane-o"></i> <h6>Headers</h6> <p>display <br> por categoria</p></div>
-        <div class="hexagon"><i class="fa fa-area-chart"></i> <h6>Estadisticas</h6> <p>indicadores <br> de sistema</p></div>
-        <div class="hexagon"><i class="fa fa-cog"></i> <h6>Cofiguración</h6> <p>ajustes de <br> sistema</p></div>
-        <div class="hexagon"><i class="fa fa-comments"></i> <h6>Alertas</h6> <p>notificaciones <br> pendientes</p></div>
-        
-        <div class="clearfix haxmargin"></div>
-        
-        <div class="hexagon two"><i class="fa fa-user"></i> <h6>Usuarios</h6> <p>administrar <br> cuentas</p></div>
-        <div class="hexagon three"><i class="fa fa-angellist"></i> <h6>Historial</h6> <p>contenidos <br> esta semana</p></div>
-        <div class="hexagon four"><i class="fa fa-bell-o"></i> <h6>Categorias</h6> <p>15 <br> categorias</p></div>
-        <div class="hexagon five"><i class="fa fa-globe"></i> <h6>Idiomas</h6> <p>2 <br> idiomas</p></div>
-
-	</div>
-    
-</div>
-</div>
+<div class="clearfix"></div><div class="parallax_section6">	<div class="container">		<h2>Admin Dashboard</h2>		<div class="box">	        <!-- <div class="hexagon"><i class="fa fa-paper-plane-o"></i> <h6>Headers</h6></div> -->	        <!-- <div class="hexagon"><i class="fa fa-area-chart"></i> <h6>Estadisticas</h6></div> -->					<div class="hexagon two" ng-click="go('Usuarios','/Usuarios')"><i class="fa fa-user"></i> <h6>Usuarios</h6></div>					<div class="hexagon three" ng-click="go('Categorias','/Categorias')"><i class="fa fa-bell-o"></i> <h6>Categorias</h6></div>					<div class="hexagon four" ng-click="go('Contenidos','/Contenidos')"><i class="fa fa-check-square-o"></i> <h6>Contenidos</h6></div>					<div class="hexagon five" ng-click="go('Idiomas','/Idiomas')"><i class="fa fa-globe"></i> <h6>Idiomas</h6></div>					<!-- <div class="hexagon"><i class="fa fa-cog"></i> <h6>Cofiguración</h6></div> -->	        <!-- <div class="hexagon"><i class="fa fa-comments"></i> <h6>Alertas</h6></div> -->	        <div class="clearfix haxmargin"></div>	        <!-- <div class="hexagon two"><i class="fa fa-user"></i> <h6>Usuarios</h6></div> -->	        <!-- <div class="hexagon three"><i class="fa fa-angellist"></i> <h6>Historial</h6></div> -->	        <!-- <div class="hexagon four"><i class="fa fa-bell-o"></i> <h6>Categorias</h6></div> -->	        <!-- <div class="hexagon five"><i class="fa fa-globe"></i> <h6>Idiomas</h6></div> -->		</div>	</div></div>
